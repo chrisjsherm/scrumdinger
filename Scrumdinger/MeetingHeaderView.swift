e1ed34915cafe193827bd3c83c3b6ad81c3183dc
Christopher Sherman <p223a@shermandigital.com>
2023-04-19 11:45:56 -0400

    ScrumProgress view

--- Scrumdinger/MeetingHeaderView.swift
+++ Scrumdinger/MeetingHeaderView.swift
@@ -26,6 +26,7 @@ struct MeetingHeaderView: View {
     var body: some View {
         VStack {
             ProgressView(value: progress)
+                .progressViewStyle(ScrumProgressViewStyle(theme: theme))
             
             HStack {
                 VStack(alignment: .leading) {
@@ -38,12 +39,14 @@ struct MeetingHeaderView: View {
                     Text("Seconds Remaining")
                         .font(.caption)
                     Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
+                        .labelStyle(.trailingIcon)
                 }
             }
         }
         .accessibilityElement(children: .ignore)
         .accessibilityLabel("Time remaining")
         .accessibilityValue("\(minutesRemaining) minutes")
+        .padding([.top, .horizontal])
     }
 }
 
