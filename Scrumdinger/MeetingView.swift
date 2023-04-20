f703162430b7f11844feb86e5a6e01528f0e5b63
Christopher Sherman <p223a@shermandigital.com>
2023-04-20 10:16:19 -0400

    Extract Meeting Footer

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -19,14 +19,8 @@ struct MeetingView: View {
                 MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: .bubblegum)
                 Circle()
                     .strokeBorder(lineWidth: 24)
-                HStack {
-                    Text("Speaker 1 of 3")
-                    Spacer()
-                    Button(action: {}) {
-                        Image(systemName: "forward.fill")
-                    }
-                    .accessibilityLabel("Next speaker")
-                }
+                
+                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
 
         }
